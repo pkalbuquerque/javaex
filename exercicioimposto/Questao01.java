4c6c151d8fd71d5339465cd994b1b28e3c2604ea
Pedro Krubniki Albuquerque <126492061+pkalbuquerque@users.noreply.github.com>
2023-09-12 08:10:29 -0300

    Delete exercicioimposto directory

--- exercicioimposto/Questao01.java
+++
@@ -1,8 +0,0 @@
-package POO.introducao.exercicioimposto;
-
-public class Questao01 {
-    public static void main(String[] args) {
-        Imposto pessoa1 = new Imposto("bolsonaro", 3000);
-        System.out.println(pessoa1.calcularIrpf());
-    }
-}
