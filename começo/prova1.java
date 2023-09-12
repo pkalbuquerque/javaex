c822b054c7879e5af359c0af22f65f45ae747a29
Pedro Krubniki Albuquerque <126492061+pkalbuquerque@users.noreply.github.com>
2023-09-12 08:10:01 -0300

    Delete começo directory

--- começo/prova1.java
+++
@@ -1,8 +0,0 @@
-package POO.introducao.começo;
-
-public class prova1 {
-    public static void main(String[] args) {
-        Pessoa umPessoa = new Pessoa("Maria", 1.80, 70 );
-        System.out.println(umPessoa.imprimir());
-    }
-}
